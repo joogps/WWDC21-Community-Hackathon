20422820e9b2c776145b0f97649cb8eb7d56fa84
joogps <joogps@gmail.com>
2021-06-12 01:03:13 -0300

    Initial Commit

---
+++ ShareBits/ShareBitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareBitsApp.swift
+//  ShareBits
+//
+//  Created by João Gabriel Pozzobon dos Santos on 12/06/21.
+//
+
+import SwiftUI
+
+@main
+struct ShareBitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
